e6bfacaeea8ac5981969aa2265e457b85888ed69
lisz1012 <lisz1012@163.com>
2021-04-01 01:27:31 -0700

    Add notes

--- src/main/java/com/lisz/hadoop/mapreduce/wc/MyMapper.java
+++ src/main/java/com/lisz/hadoop/mapreduce/wc/MyMapper.java
@@ -16,6 +16,18 @@ public class MyMapper extends Mapper<Object, Text, Text, IntWritable> {
 	// 每次序列化出来的字节数组都是新的，互不影响，map后面有一个基于内存的buffer，是一个字节数组
 	// 写在这里不用每次都new，这时候性能就节省下来了，不会对GC造成压力
 	// 见MapTask的1156行：keySerializer.serialize(key); 然后会将序列化之后的结果放入buffer
+	/*
+	final float spillper =
+        job.getFloat(JobContext.MAP_SORT_SPILL_PERCENT, (float)0.8);
+      final int sortmb = job.getInt(MRJobConfig.IO_SORT_MB,
+          MRJobConfig.DEFAULT_IO_SORT_MB);
+      溢写百分比和buffer的大小将来是要调整的
+      默认的Buffer内的排序是快排：
+      sorter = ReflectionUtils.newInstance(job.getClass("map.sort.class",
+            QuickSort.class, IndexedSorter.class), job);
+
+        Combiner的设置有时候很重要，自己可以设置
+	 */
 
 	// MapTask 794行这两个缓冲区的设置将来可以调整
 	// final float spillper =
